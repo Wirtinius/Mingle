ca22bee19760d0ed06ffb53e22fe4c9be691025c
Wirtinius <115718269+Wirtinius@users.noreply.github.com>
2024-02-05 12:46:00 +0600

    JWT implementation

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,18 +1,34 @@
-const User = require('../models/userModel')
-const Role = require('../models/roleModel')
-const bcrypt = require('bcryptjs')
+const User = require('../models/userModel');
+const Role = require('../models/roleModel');
+const bcrypt = require('bcryptjs');
+const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {secret} = require("../configuration/config");
+
+const generateAccessToken = (id, roles) => {
+    const payload =  {
+      id,
+      roles
+    }
+    return jwt.sign(payload, secret, {expiresIn: "24h"})
+}
 
 class userController {
 
   async registration (req, res) {
     try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+          return req.status(400).json({message: "Error while registration", errors})
+        }
+
         const {username, name, surname, age, email, nationality, password} = req.body
         const candidate = await User.findOne({username})
         if (candidate) {
           return res.status(400).json({message: "User with this name has already been registered!"})
         }
         const hashPassword = bcrypt.hashSync(password, 7);
-        const userRole = await Role.findOne({value: "User"})
+        const userRole = await Role.findOne({value: "USER"})
         const user = new User({username, name, surname, age, email, nationality, password: hashPassword, roles: [userRole.value]})
         await user.save()
         return res.json({message: "Succesfully registered!"})
@@ -24,6 +40,17 @@ class userController {
 
   async login (req, res) {
     try {
+      const {username, password} = req.body
+      const user = await User.findOne({username})
+      if (!user) {
+        return res.status(400).json({message: `User ${username} not found!`})
+      }
+      const validPassword = bcrypt.compareSync(password, user.password)
+      if (!validPassword) {
+        return res.status(400).json({message: "Wrong password"})
+      }
+      const token = generateAccessToken(user._id, user.roles)
+      return res.json({token}) 
 
     } catch (e) {
       console.log(e)
@@ -33,8 +60,8 @@ class userController {
 
   async getUsers (req, res) {
     try {
-
-      res.json("server works")
+      const users = await User.find()
+      res.json(users)
     } catch (e) {
       console.log(e)
     }
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const Router = require('express');
 const router = new Router()
 const userController = require('../controllers/userController')
+const {check} = require('express-validator')
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/register', userController.registration);
-router.get('/login', userController.login);
-router.get('/users', userController.getUsers);
+router.post('/register', [
+    check('username', "Username can not be empty").notEmpty(),
+    check('password', "Password be higher than 4 and less than 10").isLength({min: 4, max: 10})
+], userController.registration);
+router.post('/login', userController.login);
+router.get('/users', authMiddleware, userController.getUsers);
 
 
 module.exports = router;
